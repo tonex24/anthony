5fc4839f8c80831296c508e32d0ad9ab010c1030
tonex24 <anthonycountian7@gmail.com>
2025-01-24 17:58:09 +0800

    first commit

---
+++ java.js
@@ -0,0 +1,9 @@
+function color() {
+    var text = document.getElementById('text');
+    
+    if (text.style.color !== 'green') {
+        text.style.color = 'green';  
+    } else {
+        text.style.color = '';  
+    }
+}
